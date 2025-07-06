3b13a514366b542f09d6b6385dfd1155bde81fb8
Theo OLIVER <theo.oliver@epita.fr>
2025-07-06 11:28:32 +0200

    feat: add node red + delete useless files

--- button.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUTTON_H
-#define BUTTON_H
-
-namespace Button {
-  void button_setup();
-  void button_main();
-}
-
-#endif /* !BUTTON_H */
--- wifi.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_H
-#define WIFI_H
-
-namespace Wifi {
-  void wifi_setup();
-}
-
-#endif /* !WIFI_H */
